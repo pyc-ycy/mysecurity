db419ca837fff8be127ae9e72ac0538c66fe99b0
ycy-ycy <2923616405@qq.com>
2020-02-13 10:34:34 +0800

    update

---
+++ src/main/java/com/pyc/mysecurity/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+//IntelliJ IDEA
+//mysecurity
+//WebMvcConfig
+//2020/2/13
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.pyc.mysecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
---
+++ src/main/java/com/pyc/mysecurity/security/CustomUserService.java
@@ -0,0 +1,33 @@
+//IntelliJ IDEA
+//mysecurity
+//CustomUserService
+//2020/2/13
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.pyc.mysecurity.security;
+
+import com.pyc.mysecurity.dao.SysUserRepository;
+import com.pyc.mysecurity.domain.SysUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+// 自定义需实现 UserDetailsService 接口
+// Custom service needs to implement UserDetailsService interface
+public class CustomUserService implements UserDetailsService {
+
+    @Autowired
+    SysUserRepository userRepository;
+
+    // overwrite loadUserByUsername method to get account
+    @Override
+    public UserDetails loadUserByUsername(String username){
+        SysUser user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("用户名不存在");
+        }
+        return user;
+    }
+}
