6ca9ecc0ec60f5a1655bcfa7b41e63c2ac4e223c
ycy-ycy <2923616405@qq.com>
2020-02-13 10:04:14 +0800

    update

--- src/main/java/com/pyc/mysecurity/domain/SysRole.java
+++ src/main/java/com/pyc/mysecurity/domain/SysRole.java
@@ -8,7 +8,29 @@
 package com.pyc.mysecurity.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class SysRole {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
