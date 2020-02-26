1f458604b4a3369b627b0cc1ee441bb3193c5269
ycy-ycy <2923616405@qq.com>
2020-02-26 12:07:11 +0800

    update

--- src/main/java/com/pyc/mysecurity/MysecurityApplication.java
+++ src/main/java/com/pyc/mysecurity/MysecurityApplication.java
@@ -2,7 +2,7 @@ package com.pyc.mysecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 入口类
 @SpringBootApplication
 public class MysecurityApplication {
 
